e07bf35abc52f79d2f061fe8bf04a541ccaf4f45
bhipinder <bhupinder@gmail@gmail.com>
2023-12-16 19:02:55 -0500

    Introduction java

---
+++ src/com/takeo/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.takeo; 
+import java.time.LocalDate;
+public class HelloWorld {
+	public static void main(String... args) {
+		System.out.println("HelloWorld from New York on "+LocalDate.now()); 
+	}
+}
